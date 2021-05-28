4d98c308e1169a3c72dd42850af3e68a29b0782d
Nadya <nadya@komovich-desktop.iba>
2021-05-28 13:08:21 +0300

    fetch & add to localStorage

--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
+//     const apiUrl = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
+
+
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
-
-export default function App () {
-
-    const [appState, setAppState] = useState();
+import axios from 'axios';
+ 
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [recorded, setRecorded] = useState(true);
   
+  const fetchData = async () => {
+    const result = await axios(
+      'https://hn.algolia.com/api/v1/search?query=redux',
+    );
+    setData(result.data);
+    localStorage.setItem('Data', JSON.stringify(result.data));
+  };
+
+
   useEffect(() => {
-    const apiUrl = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
-    axios.get(apiUrl).then((resp) => {
-      const allPersons = resp.data;
-      setAppState(allPersons);
-      console.log(allPersons);
-    });
-  }, [setAppState]);
-
-  
-
+    fetchData();
+  });
  
   return (
-    <div className="app">
-        <h1>some data</h1>
-    </div>
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
   );
 }
+ 
+export default App;
