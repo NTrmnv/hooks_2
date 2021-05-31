0e3df9e720c7faedf36a8047127fde62c3eb5981
Nadya <nadya@komovich-desktop.iba>
2021-05-31 13:31:22 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
-//     const apiUrl = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
  
 function App() {
+
   const [data, setData] = useState({ hits: [] });
   const [recorded, setRecorded] = useState(false);
-  
-  //fetching data and adding to localStorage
-  const fetchData = async () => {
-    const result = await axios('https://hn.algolia.com/api/v1/search?query=redux');
-    setData(result.data);
-    setRecorded(true);
-    localStorage.setItem('Data', JSON.stringify(result.data));  
-  };
 
   useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://hn.algolia.com/api/v1/search?query=redux');     
+      localStorage.setItem('Data', JSON.stringify(result.data));
+      setRecorded(true);
+    }
     fetchData();
-    if(recorded)
-       console.log(localStorage.getItem('Data'));
-  },[recorded]);
+  });
+  
+
+  useEffect(() => {
+    if(recorded){
+      const getData = async () => {
+        const result = await axios('https://hn.algolia.com/api/v1/search?query=redux');     
+        setData(result.data);
+        JSON.parse(localStorage.getItem('Data', result.data));
+      }
+      getData();
+    }
+  }, [recorded]);
  
+
   return (
     <ul>
       {data.hits.map(item => (
