e884798ecc939420650acfb834ca9d05810486ba
Nadeshda <nadiakomovich@gmail.com>
2021-06-04 21:29:14 +0300

    data.js created & useEffect fixed

--- src/app.js
+++ src/app.js
@@ -1,43 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
  
+import Data from './data.js';
+
 function App() {
-
-  const [data, setData] = useState({ hits: [] });
-  const [recorded, setRecorded] = useState(false);
-
-  const url = 'https://hn.algolia.com/api/v1/search?query=redux';
-  const fetchData = async () => {
-    const result = await axios(url);     
-    localStorage.setItem('Data', JSON.stringify(result.data));
-    setRecorded(true);
-  }
-  const postData = async () => {
-    const result = await axios(url);     
-    setData(result.data);
-    JSON.parse(localStorage.getItem('Data', result.data));
-  }
-
-  useEffect(() => {
-    fetchData();
-  });
-
-  useEffect(() => {
-    if(recorded){
-      postData();
-    }
-  }, [recorded]);
- 
-
-  return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
+  return <Data/>
 }
  
 export default App;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Data () {
+    
+  const [data, setData] = useState({ hits: [] });
+  const [recorded, setRecorded] = useState(false);
+
+  const url = 'https://hn.algolia.com/api/v1/search?query=redux';
+  const fetchData = async () => {
+    const result = await axios(url);     
+    localStorage.setItem('Data', JSON.stringify(result.data));
+    setRecorded(true);
+  }
+  const postData = async () => {
+    const result = await axios(url);     
+    setData(result.data);
+    JSON.parse(localStorage.getItem('Data', result.data));
+  }
+
+  useEffect(() => {
+    fetchData();
+    console.log(1);
+  }, []);
+
+  useEffect(() => {
+    if(recorded){
+      postData();
+    }
+  }, [recorded]);
+ 
+
+  return (
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Data;
